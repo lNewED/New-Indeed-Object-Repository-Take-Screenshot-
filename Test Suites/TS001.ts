c6d4c430a588244f8373d58cd64e58a9ecf36e96
lNewED <NewHaZao.4@gmail.com>
2023-04-02 00:33:27 +0700

    first commit

---
+++ Test Suites/TS001.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>128b24ac-b7d1-44bc-9e87-f13d6efdbf44</testSuiteGuid>
+   <testCaseLink>
+      <guid>80ce3e6d-f2b4-4d7e-841c-a5f583a8b1e6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC03-SearchJob</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
